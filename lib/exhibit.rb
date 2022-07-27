3b0960188b08941a28e481444bb413ced7342848
Kevin Ta <kvnta@yahoo.com>
2022-07-27 09:48:28 -0600

    Initial set up for patron_spec

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -2,8 +2,8 @@ class Exhibit
     attr_reader :name,   
                 :cost 
     def initialize(hash)
-        @name = hash{:name}
-        @cost = hash{:cost} 
+        @name = hash[:name]
+        @cost = hash[:cost] 
     end
 
 
