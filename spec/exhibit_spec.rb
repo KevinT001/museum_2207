85168602326777688766df00e8486f7a5f6b0688
Kevin Ta <kvnta@yahoo.com>
2022-07-27 09:24:28 -0600

    Test exists

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Exhibit do
         @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     end
 
-    
+    it 'exist' do 
+        expect(@exhibit).to  be_a(Exhibit)
+    end
 
 end
